e6cd13431e9d085a126edb2bc71d228739104cca
ihakkiucar <ismailhucar@gmail.com>
2021-05-09 04:32:27 +0300

    filter excerpt

--- functions.php
+++ functions.php
@@ -188,3 +188,20 @@ function my_deregister_scripts(){
 	wp_deregister_script( 'wp-embed' );
   }
   add_action( 'wp_footer', 'my_deregister_scripts' );
+
+    /**
+ * Filter the except length to 20 words.
+ *
+ * @param int $length Excerpt length.
+ * @return int (Maybe) modified excerpt length.
+ */
+function wpdocs_custom_excerpt_length( $length ) {
+    return 20;
+}
+add_filter( 'excerpt_length', 'wpdocs_custom_excerpt_length', 999 );
+
+/* Excerpt last characters */
+function wpdocs_excerpt_more( $more ) {
+    return '...';
+}
+add_filter( 'excerpt_more', 'wpdocs_excerpt_more' );
